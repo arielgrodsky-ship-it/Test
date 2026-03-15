fc6ba3d4f51dc496afc981c2554ea2b1f20d560c
ariel . <groariel@gmail.com>
2026-03-15 11:38:34 +0200

    Initial commit

---
+++ FrontEnd/contact.js
@@ -0,0 +1,71 @@
+function validateForm() {
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const age = document.getElementById("age").value;
+    const phone = document.getElementById("Tel").value;
+    const issue = document.getElementById("Issue").value;
+
+    const usernamePattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9_]+$/;
+
+    if (username.trim() === "") {
+        alert("Please enter a username!");
+        return; 
+    }
+
+    if( username.length < 3 || username.length > 20) {
+        alert("Username must be between 3 and 20 characters!");
+        return;
+    }
+
+    if(!usernamePattern.test(username)) {
+        alert("The username must contain one Cap letter, one small letter, and one number.");
+        return;
+    }
+
+
+    const basicEmailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!basicEmailPattern.test(email)) {
+        alert("Invalid email! Make sure it contains an @ and .");
+        return;
+    }
+
+    if (age < 15 || age >= 120) {
+        alert("Age must be at least 15 and less than 120!");
+        return;
+    }
+
+    const phonePattern = /^[0-9]{10}$/; 
+    if (!phonePattern.test(phone)) {
+        alert("Please enter a valid 10-digit phone number!");
+        return;
+    }
+
+    if (issue === "") {
+        alert("Please select an issue!");
+        return;
+    }
+
+    const submitBtn = document.getElementById("submitBtn");
+
+    // GET – שולח בקשה עם נתונים ב-URL
+    submitBtn.addEventListener("click", () => {
+        fetch("http://localhost:3000/get?username=" + username + "&email=" + email + "&age=" + age + "&phone=" + phone + "&issue=" + issue)
+            .then(res => res.text())
+            .then(data => alert(data))
+            .catch(err => console.error(err));
+    });
+
+    // POST – שולח בקשה עם נתונים בגוף הבקשה
+    submitBtn.addEventListener("click", () => {
+        fetch("http://localhost:3000/post", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name: username, email: email , phone: phone})
+        })
+        .then(res => res.text())
+        .then(data => alert(data))
+        .catch(err => console.error(err));
+});
+}
